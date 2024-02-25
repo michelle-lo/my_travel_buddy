32aefa0fde50ea4c56990a3c7aebc393fd2a0204
Michelle Lo <mcihello04@gmail.com>
2024-02-24 23:57:17 -0500

    basic framework

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return render_template('home.html')
+# app = Flask(__name__)
+# @app.route('/')
+# def hello_world():
+#     return render_template('home.html')
 
 
-if __name__ == '__main__': 
-    app.run(host='0.0.0.0', debug=True)
+# if __name__ == '__main__': 
+#     app.run(host='0.0.0.0', debug=True)
+
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
