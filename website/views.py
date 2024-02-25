df5165405f426de9ca2275a3d8d444ed71bee3ee
Laura Z <122897510+lwz9LauraZ@users.noreply.github.com>
2024-02-25 02:25:52 -0500

    dashboard and profile

--- website/views.py
+++ website/views.py
@@ -6,3 +6,13 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html")
 #"<h1>Test</h1>"
+
+@views.route('/personal_info')
+def home2():
+    return render_template("get_personal_info.html")
+#"<h1>Test</h1>"
+
+@views.route('/dashboard')
+def home3():
+    return render_template("dashboard.html")
+#"<h1>Test</h1>"
