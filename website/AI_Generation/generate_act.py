74aca85673f11cb4af988cb175f3188fd2a97183
Laura Z <122897510+lwz9LauraZ@users.noreply.github.com>
2024-02-25 04:44:22 -0500

    new trip, profile settings, profile, (in prog) trip

---
+++ website/AI_Generation/generate_act.py
@@ -0,0 +1,2 @@
+import recombee_api_client
+client = recombee_api_client.Client('ucIdhtB7kLeuPRgrw7DUg8TAJLkgrkD3acfd2FDGInMm3x8PWGW1dyd3OaPvnoxh', 'my-travel-buddy-dev')
--- website/views.py
+++ website/views.py
@@ -16,3 +16,23 @@ def home2():
 def home3():
     return render_template("dashboard.html")
 #"<h1>Test</h1>"
+
+@views.route('/new_trip')
+def new_trip():
+    return render_template("new_trip.html")
+#"<h1>Test</h1>"
+
+@views.route('/profile')
+def profile():
+    return render_template("profile.html")
+#"<h1>Test</h1>"
+
+@views.route('/profile_settings')
+def profile_settings():
+    return render_template("profile_settings.html")
+#"<h1>Test</h1>"
+
+@views.route('/trip')
+def trip():
+    return render_template("trip.html")
+#"<h1>Test</h1>"
